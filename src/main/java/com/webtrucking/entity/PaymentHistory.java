4e0abae2ad45dde558224d82bdcd9997d18c21ba
Vu Quoc Dat <dat.vu@ascendcorp.com>
2017-08-17 23:24:50 +0700

    display map

---
+++ src/main/java/com/webtrucking/entity/PaymentHistory.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.webtrucking.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author vuquocdat
+ */
+@Entity
+@Table(name = "payment_history")
+@XmlRootElement
+public class PaymentHistory implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "user_id")
+    private int userId;
+    @Basic(optional = false)
+    @Column(name = "wallet_id")
+    private int walletId;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Basic(optional = false)
+    @Column(name = "total_amount")
+    private BigDecimal totalAmount;
+    @Basic(optional = false)
+    @Column(name = "status")
+    private boolean status;
+    @Column(name = "created_timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdTimestamp;
+    @Column(name = "last_updated_timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastUpdatedTimestamp;
+    @Column(name = "order_id")
+    private Integer orderId;
+
+    public PaymentHistory() {
+    }
+
+    public PaymentHistory(Integer id) {
+        this.id = id;
+    }
+
+    public PaymentHistory(Integer id, int userId, int walletId, BigDecimal totalAmount, boolean status) {
+        this.id = id;
+        this.userId = userId;
+        this.walletId = walletId;
+        this.totalAmount = totalAmount;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getWalletId() {
+        return walletId;
+    }
+
+    public void setWalletId(int walletId) {
+        this.walletId = walletId;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getCreatedTimestamp() {
+        return createdTimestamp;
+    }
+
+    public void setCreatedTimestamp(Date createdTimestamp) {
+        this.createdTimestamp = createdTimestamp;
+    }
+
+    public Date getLastUpdatedTimestamp() {
+        return lastUpdatedTimestamp;
+    }
+
+    public void setLastUpdatedTimestamp(Date lastUpdatedTimestamp) {
+        this.lastUpdatedTimestamp = lastUpdatedTimestamp;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof PaymentHistory)) {
+            return false;
+        }
+        PaymentHistory other = (PaymentHistory) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "javaapplication1.PaymentHistory[ id=" + id + " ]";
+    }
+    
+}
